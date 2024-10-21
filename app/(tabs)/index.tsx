7c2f5c938763d4276a1722a8e2abb235f713329a
rabailkhan321 <FA21-BSE-036@cuilahore.edu.pk>
2024-10-21 13:05:05 +0500

    rechecking after initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,14 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { FlatList } from 'react-native-reanimated/lib/typescript/Animated';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello</Text>
+      <Text style={styles.title}>Hello world</Text>
+      
 
     </View>
   );
